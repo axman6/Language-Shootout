1ace0b907bcc25eab7db2aedd01773a80f505ef3
Alex Mason <axman6@gmail.com>
2011-01-17 16:41:07 +0900

    Made things slightly more parallel when eval'ing long
    Worker wrapper transformations. I don't expect these to help much.
    `check` is now more parallel also.

--- binary-trees/new.hs
+++ binary-trees/new.hs
@@ -33,35 +33,42 @@ io s n t = printf "%s of depth %d\t check: %d\n" s n t
 main :: IO ()
 main = do
     n <- getArgs >>= readIO . head
-    let maxN     = max (minN + 2) n
-        stretchN = maxN + 1
-        c = check (make 0 stretchN)
+    let maxN        = max (minN + 2) n
+        stretchN    = maxN + 1
+        c           = check (make 0 stretchN)
         -- allocate a long lived tree
-        !long    = make 0 maxN
+        long       = make 0 maxN
         -- allocate, walk, and deallocate many bottom-up binary trees
-        vs = depth minN maxN `using` parListN (numCapabilities*2) rdeepseq
+        vs          = depth minN maxN `using` parList rdeepseq
     -- ensure long is evaluates first
-    long `seq` io "stretch tree" stretchN c
+    c `par`long `pseq` io "stretch tree" stretchN c
     mapM_ (\((m,d,i)) -> io (show m ++ "\t trees") d i) vs
     -- confirm the the long-lived binary tree still exists
     io "long lived tree" maxN (check long)
 
 -- generate many trees
 depth :: Int -> Int -> [(Int,Int,Int)]
-depth d m
-    | d <= m    = (2*n,d,sumT d n 0) : depth (d+2) m
-    | otherwise = []
-  where n = 1 `shiftL` (m - d + minN)
+depth d' m = go d' where
+    go d | d <= m    = (2*n,d,sumT d n 0) : depth (d+2) m
+         | otherwise = []
+         where !n = 1 `shiftL` (m - d + minN)
 
 -- allocate and check lots of trees
 sumT :: Int -> Int -> Int -> Int
-sumT _ 0 t = t
-sumT  d i t = sumT d (i-1) (t + a + b)
-  where a = check (make i    d)
-        b = check (make (-i) d)
+sumT d i' t' = go i' t' where
+    go 0 t = t
+    go i t =  a `par` b `pseq` go (i-1) $! (t + a + b)
+      where a = check (make i    d)
+            b = check (make (-i) d)
 
 check :: Tree -> Int
-check arr = go 1 where
+check arr = pgo 1 where
+    pgo i | i < 4 = l `par` r `pseq` indx i + l - r
+          | otherwise = go i where
+              l = pgo i'
+              r = pgo (i'+1)
+              !i' = i+i
+
     go !i | i >= end = 0
     go i = let !i' = i+i
                l = go $ i'   -- left child
@@ -76,7 +83,7 @@ make i d = runSTUArray $ do
     arr <- newArray (1,end) 0
     writeA arr 1 i
     forM_ [2..end] $ \ix -> do
-        p <- readA arr $ shiftR ix 1 -- parent
+        p <- readA arr $ ix `shiftR` 1 -- parent
         writeA arr  ix $! p + p + (if even ix then -1 else 0)
     return arr where
     readA arr ix = unsafeRead arr (ix-1)
